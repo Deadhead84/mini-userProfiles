3bd0043f35df8d39115df2b777049930af5e310a
Deadhead84 <Jon@Scoville-MacBook.local>
2015-08-19 06:52:21 -0600

    syncing my repos from earlier this year

---
+++ controller.js
@@ -0,0 +1,15 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+
+  var getUsers = function() {
+  	mainService.getUsers().then(function(data) {
+  		$scope.users = data;
+  		console.log($scope.users);
+  	});
+  }
+
+  getUsers();
+
+  
+});
